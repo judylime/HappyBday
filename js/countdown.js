ce52ba625528eeef42c5ebe75b129ce4def49be7
JudyLime <56984748+judylime@users.noreply.github.com>
2020-06-23 16:20:20 -0700

    img and file name changed

--- js/countdown.js
+++ js/countdown.js
@@ -25,7 +25,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Dear sister|祝你|生日快乐|happy birthday|#countdown 3||');
+      S.UI.simulate('Dear Sister|祝你|生日快乐|happy birthday|#countdown 3||');
     }
 
     S.Drawing.loop(function () {
